661a0f6b8c0c515d403e37f2e897a9707a48203a
jonathantyar <jonathantyar@gmail.com>
2020-12-15 10:21:28 +0700

    adding db:seed for populate dummy data to database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Section;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Section::factory()
+            ->times(10)
+            ->hasTasks(3)
+            ->create();
     }
 }
